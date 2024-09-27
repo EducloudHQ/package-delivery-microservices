efef4ae2614921c4c7b12bc2e7c65fbc26ea2ee5
fonchu <hintekk@fonchu.local>
2024-09-27 07:44:27 +0100

    initial commit

--- lib/merged-api-stack.ts
+++ lib/merged-api-stack.ts
@@ -1,16 +1,52 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+
+import * as cdk from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {CfnGraphQLApi} from "aws-cdk-lib/aws-appsync";
+import {Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import {CfnOutput} from "aws-cdk-lib";
+
+const NAME = 'PackageDeliveryMergedApi'
 export class MergedApiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  private packageDeliveryMergedApi: CfnGraphQLApi;
+
+  constructor(scope: Construct, id: string, props: cdk.StageProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const executionRole = new Role(this, 'MergedApiExecutionRole', {
+      assumedBy: new ServicePrincipal('appsync.amazonaws.com')
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MergedApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    this.packageDeliveryMergedApi = new CfnGraphQLApi(this, `${NAME}`, {
+      authenticationType: "API_KEY",
+      name: `${NAME}`,
+      apiType: 'MERGED',
+      mergedApiExecutionRoleArn: executionRole.roleArn,
+      additionalAuthenticationProviders: [
+        {
+          authenticationType: 'AWS_IAM'
+        },
+      ],
+    });
+
+    new CfnOutput(this, `${NAME}Url`, {
+      exportName: `${props.stageName}-${NAME}Url`,
+      value: this.packageDeliveryMergedApi.attrGraphQlUrl
+    });
+
+    new CfnOutput(this, `${NAME}Arn`, {
+      exportName: `${props.stageName}-${NAME}Arn`,
+      value: this.packageDeliveryMergedApi.attrArn
+    })
+
+    new CfnOutput(this, `${NAME}Id`, {
+      exportName: `${props.stageName}-${NAME}Id`,
+      value: this.packageDeliveryMergedApi.attrApiId
+    });
+
+    new CfnOutput(this, `${NAME}ArnExecutionRole`, {
+      exportName: `${props.stageName}-${NAME}ExecutionRoleArn`,
+      value: executionRole.roleArn
+    })
   }
 }
