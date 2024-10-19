d7a664c086814d0a91ee0d5ba22769f6e85b4075
trey-rosius <treyrosius@gmail.com>
2024-10-19 13:59:35 +0100

    more updates

--- lib/merged-api-stack.ts
+++ lib/merged-api-stack.ts
@@ -1,52 +1,50 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { CfnGraphQLApi } from "aws-cdk-lib/aws-appsync";
+import { Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { CfnOutput } from "aws-cdk-lib";
 
-
-import * as cdk from 'aws-cdk-lib';
-import {Construct} from 'constructs';
-import {CfnGraphQLApi} from "aws-cdk-lib/aws-appsync";
-import {Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
-import {CfnOutput} from "aws-cdk-lib";
-
-const NAME = 'PackageDeliveryMergedApi'
+const NAME = "PackageDeliveryMergedApi";
 export class MergedApiStack extends cdk.Stack {
   private packageDeliveryMergedApi: CfnGraphQLApi;
 
   constructor(scope: Construct, id: string, props: cdk.StageProps) {
     super(scope, id, props);
 
-    const executionRole = new Role(this, 'MergedApiExecutionRole', {
-      assumedBy: new ServicePrincipal('appsync.amazonaws.com')
+    const executionRole = new Role(this, "MergedApiExecutionRole", {
+      assumedBy: new ServicePrincipal("appsync.amazonaws.com"),
     });
 
     this.packageDeliveryMergedApi = new CfnGraphQLApi(this, `${NAME}`, {
       authenticationType: "API_KEY",
       name: `${NAME}`,
-      apiType: 'MERGED',
+      apiType: "MERGED",
       mergedApiExecutionRoleArn: executionRole.roleArn,
       additionalAuthenticationProviders: [
         {
-          authenticationType: 'AWS_IAM'
+          authenticationType: "AWS_IAM",
         },
       ],
     });
 
     new CfnOutput(this, `${NAME}Url`, {
       exportName: `${props.stageName}-${NAME}Url`,
-      value: this.packageDeliveryMergedApi.attrGraphQlUrl
+      value: this.packageDeliveryMergedApi.attrGraphQlUrl,
     });
 
     new CfnOutput(this, `${NAME}Arn`, {
       exportName: `${props.stageName}-${NAME}Arn`,
-      value: this.packageDeliveryMergedApi.attrArn
-    })
+      value: this.packageDeliveryMergedApi.attrArn,
+    });
 
     new CfnOutput(this, `${NAME}Id`, {
       exportName: `${props.stageName}-${NAME}Id`,
-      value: this.packageDeliveryMergedApi.attrApiId
+      value: this.packageDeliveryMergedApi.attrApiId,
     });
 
     new CfnOutput(this, `${NAME}ArnExecutionRole`, {
       exportName: `${props.stageName}-${NAME}ExecutionRoleArn`,
-      value: executionRole.roleArn
-    })
+      value: executionRole.roleArn,
+    });
   }
 }
